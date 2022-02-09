30bad49085af4aceef1b1d175c4255ffd6c22ec4
Kelly Lee <amorfati38@gmail.com>
2022-02-09 00:24:04 -0700

    test6

--- README.md
+++ README.md
@@ -1,40 +1,32 @@
 
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <title>1</title>
-    </head>
-    <body>
-      <h1># Project Title: 1</h1> 
-        <p>
+    <p>
   <img src="https://img.shields.io/badge/License-Apache_2.0-blue.svg">
   </p>
-      <h2>## Description</h2>
-          <p>2</p>
-      <h2>## Table of Contents</h2>
-          <ul>
-              <li><a href="#installation">Installation</a></li>
-              <li><a href="#usage">Usage</a></li>
-              <li><a href="#contribution">Contribution</a></li>
-              <li><a href="#test">Test</a></li>
-              <li><a href="#license">License</a></li>
-              <li><a href="#questions">Questions</a></li>
-          </ul>
-      <h2 id="installation">## Installation</h2>
-          <p>3</p>
-      <h2 id="usage">## Usage</h2>
-          <p></p>
-      <h2 id="contribution">## Contribution</h2>
-          <p>44</p>
-      <h2 id="test">## Test</h2>
-          <p>4</p>
-      <h2 id="license">## License</h2>
-          <p>
+    <h1># 1</h1> 
+    <h2>## Description</h2>
+        <p>2</p>
+    <h2>## Table of Contents</h2>
+        <ul>
+            <li><a href="#installation">Installation</a></li>
+            <li><a href="#usage">Usage</a></li>
+            <li><a href="#contribution">Contribution</a></li>
+            <li><a href="#test">Test</a></li>
+            <li><a href="#license">License</a></li>
+            <li><a href="#questions">Questions</a></li>
+        </ul>
+    <h2 id="installation">## Installation</h2>
+        <p>3</p>
+    <h2 id="usage">## Usage</h2>
+        <p>4</p>
+    <h2 id="contribution">## Contribution</h2>
+        <p>5</p>
+    <h2 id="test">## Test</h2>
+        <p>5</p>
+    <h2 id="license">## License</h2>
+        <p>
     This application is covered under Apache_2.0 and please <a href="https://choosealicense.com/licenses/">click here</a> to find out more details.
   </p>
-      <h2 id="questions">## Questions</h2>
-          <p><a href="https://github.com/4">Github</a></p>
-          <p>If you have additional questions, please <a href="4">"Reach Me".</a><p>            
-    </body>
-    </html>
+    <h2 id="questions">## Questions</h2>
+        <p><a href="https://github.com/4">Github</a></p>
+        <p>If you have additional questions, please <a href="4">"Reach Me".</a><p>            
   
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -25,40 +25,32 @@ function renderLicenseLink(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <title>${data.title}</title>
-    </head>
-    <body>
-      <h1># Project Title: ${data.title}</h1> 
-        <p>${renderLicenseBadge(data.license)}</p>
-      <h2>## Description</h2>
-          <p>${data.description}</p>
-      <h2>## Table of Contents</h2>
-          <ul>
-              <li><a href="#installation">Installation</a></li>
-              <li><a href="#usage">Usage</a></li>
-              <li><a href="#contribution">Contribution</a></li>
-              <li><a href="#test">Test</a></li>
-              <li><a href="#license">License</a></li>
-              <li><a href="#questions">Questions</a></li>
-          </ul>
-      <h2 id="installation">## Installation</h2>
-          <p>${data.installation}</p>
-      <h2 id="usage">## Usage</h2>
-          <p>${data.usage}</p>
-      <h2 id="contribution">## Contribution</h2>
-          <p>${data.contribution}</p>
-      <h2 id="test">## Test</h2>
-          <p>${data.test}</p>
-      <h2 id="license">## License</h2>
-          <p>${renderLicenseLink(data.license)}</p>
-      <h2 id="questions">## Questions</h2>
-          <p><a href="https://github.com/${data.github}">Github</a></p>
-          <p>If you have additional questions, please <a href="${data.email}">"Reach Me".</a><p>            
-    </body>
-    </html>
+    <p>${renderLicenseBadge(data.license)}</p>
+    <h1># ${data.title}</h1> 
+    <h2>## Description</h2>
+        <p>${data.description}</p>
+    <h2>## Table of Contents</h2>
+        <ul>
+            <li><a href="#installation">Installation</a></li>
+            <li><a href="#usage">Usage</a></li>
+            <li><a href="#contribution">Contribution</a></li>
+            <li><a href="#test">Test</a></li>
+            <li><a href="#license">License</a></li>
+            <li><a href="#questions">Questions</a></li>
+        </ul>
+    <h2 id="installation">## Installation</h2>
+        <p>${data.installation}</p>
+    <h2 id="usage">## Usage</h2>
+        <p>${data.usage}</p>
+    <h2 id="contribution">## Contribution</h2>
+        <p>${data.contribution}</p>
+    <h2 id="test">## Test</h2>
+        <p>${data.test}</p>
+    <h2 id="license">## License</h2>
+        <p>${renderLicenseLink(data.license)}</p>
+    <h2 id="questions">## Questions</h2>
+        <p><a href="https://github.com/${data.github}">Github</a></p>
+        <p>If you have additional questions, please <a href="${data.email}">"Reach Me".</a><p>            
   `;
 }
 
